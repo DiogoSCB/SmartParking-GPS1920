6d7f27008a0714de7771d3b43fc5b20950fe2c76
JalxP <jalxp@icloud.com>
2019-11-27 12:10:11 +0000

    First UI mockup defined, added some basic callbacks to buttons.

---
+++ SmartParking/src/View/Constants.java
@@ -0,0 +1,12 @@
+package View;
+
+public interface Constants
+{
+    // Misc
+    String fxmlFileName = "ui.fxml";
+
+    // UI Windows
+    String windowName = "Smart Parking 0.1";
+    int windowWidth = 1024;
+    int windowHeight = 768;
+}
