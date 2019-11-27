57a73963a04c420e3b0026314cb5606f32a9a29e
JalxP <jalxp@icloud.com>
2019-11-27 12:28:32 +0000

    First UI mockup defined, added some basic callbacks to buttons. Created basic .gitignore file"

---
+++ SmartParking/src/View/UIConstants.java
@@ -0,0 +1,13 @@
+package View;
+
+public interface UIConstants
+{
+    // FXML settings
+    String fxmlFileName = "ui.fxml";
+
+    // Window settings
+    String windowName = "Smart Parking 0.1";
+
+    int windowWidth = 1024;
+    int windowHeight = 768;
+}
